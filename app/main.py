9e27daff55a621ceb9e2f754bc6901f20895c423
Aleksandr-WA <ShirshovAleksandrWA@yandex.ru>
2024-09-09 15:46:08 +0300

    Revert "add error handler"
    This reverts commit 49fcd39c75b68d932d78b1a9a344cc4fa1d5fdcf.

--- app/main.py
+++ app/main.py
@@ -2,25 +2,15 @@ import uvicorn
 from api import router as api_router
 from core.config import settings
 from create_fastapi_app import create_app
-from errors.errors import (
-    validation_exception_handler,
-    http_exception_handler,
-    general_exception_handler,
-)
-from fastapi import HTTPException
-from fastapi.exceptions import RequestValidationError
 
 main_app = create_app(
     create_custom_static_urls=True,
 )
 
-main_app.add_exception_handler(RequestValidationError, validation_exception_handler)
-main_app.add_exception_handler(HTTPException, http_exception_handler)
-main_app.add_exception_handler(Exception, general_exception_handler)
-
 main_app.include_router(
     api_router,
 )
+
 
 if __name__ == "__main__":
     uvicorn.run(
