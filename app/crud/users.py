e6576d8aa3e8a812f454e1901295275e7c5f7b64
Aleksandr-WA <ShirshovAleksandrWA@yandex.ru>
2024-09-09 15:46:08 +0300

    Revert "create crud"
    This reverts commit 1974f8d6a2b754726499d254fe9a4630f6ec2b83.

---
+++ app/crud/users.py
@@ -0,0 +1,24 @@
+from typing import Sequence
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from models.parcels import Parcel
+from schemas.parcels import UserCreate
+
+
+async def get_all_users(
+    session: AsyncSession,
+) -> Sequence[Parcel]:
+    stmt = select(Parcel).order_by(Parcel.id)
+    result = await session.scalars(stmt)
+    return result.all()
+
+
+async def create_user(
+    session: AsyncSession,
+    user_create: UserCreate,
+) -> Parcel:
+    user = Parcel(**user_create.model_dump())
+    session.add(user)
+    await session.commit()
+    # await session.refresh(user)
+    return user
